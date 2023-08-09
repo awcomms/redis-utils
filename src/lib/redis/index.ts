20d36986c1140351222c2fe954499d6361736089
EdwardGold Edem Hogan <edge37@outlook.com>
2023-08-09 16:26:31 +0000

    .

--- src/lib/redis/index.ts
+++ src/lib/redis/index.ts
@@ -4,3 +4,4 @@ export { get } from './get.js';
 export { object_value } from './object_value.js';
 export { search } from './search.js';
 export { update } from './update.js';
+export * from './shape/index.js'
